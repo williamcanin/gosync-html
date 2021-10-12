ce075be29391cd9f43d027f2f30498430ed78409
William C. Canin <william.costa.canin@gmail.com>
2021-10-11 23:32:45 -0300

    Auto commit - D:11102021-H:233245

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -5,7 +5,7 @@ $(".alert").fadeTo(2000, 500).slideUp(500, function(){
 });
 
 // Add Popovers (Bootstrap)
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl);
 })
